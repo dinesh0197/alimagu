ac78f58b3f52eb78a76df01f9e4c4c2339f2df65
dinesh <dineshr0197@gmail.com>
2023-12-14 22:32:08 +0530

    modified publication api response

--- src/api/publications-page/controllers/publications-page.js
+++ src/api/publications-page/controllers/publications-page.js
@@ -12,11 +12,11 @@ module.exports = createCoreController('api::publications-page.publications-page'
         // Calling the default core action
         const { data, meta } = await super.find(ctx);
     
-        const extractedData = data?.attributes.map(item => ({
-            title: item?.title,
-            publication_date: item?.publication_date,
-            description: item?.description,
-            image: item?.image?.data?.attributes?.url
+        const extractedData = data.map(item => ({
+            title: item.attributes.title,
+            publication_date: item.attributes.publication_date,
+            description: item.attributes.description,
+            image: item.attributes.image.data.attributes.url
           }));
     
         return { data: extractedData, meta };
