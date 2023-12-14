73216432a1a0effaedc70e3851550d1a3422b3a1
dinesh <dineshr0197@gmail.com>
2023-12-14 22:16:29 +0530

    modified publication api response

--- src/api/publications-page/controllers/publications-page.js
+++ src/api/publications-page/controllers/publications-page.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::publications-page.publications-page');
+module.exports = createCoreController('api::publications-page.publications-page',({strapi})=>({
+    async find(ctx) {
+    
+        // Calling the default core action
+        const { data, meta } = await super.find(ctx);
+    
+        const extractedData = data.map(item => ({
+            title: item.attributes.title,
+            publication_date: item.attributes.publication_date,
+            description: item.attributes.description,
+            image: item.attributes.image.data.attributes.url
+          }));
+    
+        return { data: extractedData, meta };
+      },
+}));
