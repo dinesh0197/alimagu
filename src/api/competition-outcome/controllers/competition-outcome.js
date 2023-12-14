de65c8e0c8a3206fc1ef31e45074b8f8fae89da4
dinesh <dineshr0197@gmail.com>
2023-12-14 22:54:30 +0530

    modified competition_outcome response

--- src/api/competition-outcome/controllers/competition-outcome.js
+++ src/api/competition-outcome/controllers/competition-outcome.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::competition-outcome.competition-outcome');
+module.exports = createCoreController('api::competition-outcome.competition-outcome',({strapi})=>({
+    async find(ctx) {
+    
+        // Calling the default core action
+        const { data, meta } = await super.find(ctx);
+    
+        const extractedData = data.map(item => ({
+            description: item?.attributes?.description,
+            title: item?.attributes?.title
+          }));
+    
+        return { data: extractedData, meta };
+      },
+}));
--- src/api/publications-page/controllers/publications-page.js
+++ src/api/publications-page/controllers/publications-page.js
@@ -13,10 +13,10 @@ module.exports = createCoreController('api::publications-page.publications-page'
         const { data, meta } = await super.find(ctx);
     
         const extractedData = data.map(item => ({
-            title: item.attributes.title,
-            publication_date: item.attributes.publication_date,
-            description: item.attributes.description,
-            image: item.attributes.image.data.attributes.url
+            title: item?.attributes?.title,
+            publication_date: item?.attributes?.publication_date,
+            description: item?.attributes?.description,
+            image: item?.attributes?.image?.data?.attributes?.url
           }));
     
         return { data: extractedData, meta };
