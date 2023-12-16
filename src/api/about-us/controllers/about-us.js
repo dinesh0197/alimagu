8a45eeccde2274f8e274701e9fd1bc6e4b108d42
dinesh <dineshr0197@gmail.com>
2023-12-16 14:36:30 +0530

    modified about us and new letter response format

--- src/api/about-us/controllers/about-us.js
+++ src/api/about-us/controllers/about-us.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about-us.about-us');
+module.exports = createCoreController('api::about-us.about-us',({strapi})=>({
+    async find(ctx) {
+        const baseUrl = process.env.STRAPI_ADMIN_BASE_URL
+
+        const data = await strapi.query('api::about-us.about-us').findMany({
+            populate: {
+              image: true,
+            }
+        });
+        const extractedData = data.map(item => ({
+                ...item,
+                image: item?.image?.url ? `${baseUrl}${item?.image?.url}`: null
+          }));
+    
+        return {data: extractedData};
+      },
+}));
--- src/api/news-letter/controllers/news-letter.js
+++ src/api/news-letter/controllers/news-letter.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::news-letter.news-letter');
+module.exports = createCoreController('api::news-letter.news-letter',({strapi})=>({
+    async find(ctx) {
+        const baseUrl = process.env.STRAPI_ADMIN_BASE_URL
+
+        const data = await strapi.query('api::news-letter.news-letter').findMany({
+            populate: {
+              image: true,
+            }
+        });
+        const extractedData = data.map(item => ({
+                ...item,
+                image: item?.image?.url ? `${baseUrl}${item?.image?.url}`: null
+          }));
+    
+        return {data: extractedData};
+      },
+}));
