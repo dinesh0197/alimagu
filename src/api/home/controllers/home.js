54ad74230d261a63d1664891cc458633e33b0996
dinesh <dineshr0197@gmail.com>
2023-12-16 12:55:37 +0530

    modified home controller

--- src/api/home/controllers/home.js
+++ src/api/home/controllers/home.js
@@ -6,4 +6,38 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::home.home');
+module.exports = createCoreController('api::home.home',({strapi})=>({
+    async find(ctx) {
+        const baseUrl = process.env.STRAPI_ADMIN_BASE_URL
+        // Calling the default core action
+        // const { data, meta } = await super.find(ctx);
+
+        const data = await strapi.query('api::home.home').findMany({
+          populate: true,
+        });
+        // const extractedData = data.map(item => ({
+        //     main : {
+        //         description: item?.attributes?.description,
+        //         image: item?.attributes?.image?.data?.attributes?.url ? `${baseUrl}${item?.attributes?.image?.data?.attributes?.url}`: null
+        //     },
+        //     home_event_and_programs: item?.attributes?.home_event_and_programs?.data.map(events => ({
+        //         title: events?.attributes?.title,
+        //         description: events?.attributes?.description,
+        //         image: item?.attributes?.image?.data?.attributes?.url ? `${baseUrl}${item?.attributes?.image?.data?.attributes?.url}`: null
+        //     })),
+        //     home_newsletters: item?.attributes?.home_newsletters?.data.map(events => ({
+        //         title: events?.attributes?.title,
+        //         description: events?.attributes?.description,
+        //         image: item?.attributes?.image?.data?.attributes?.url ? `${baseUrl}${item?.attributes?.image?.data?.attributes?.url}`: null
+        //     })),
+        //     home_membership: item?.attributes?.home_membership?.data.map(events => ({
+        //         title: events?.attributes?.title,
+        //         description: events?.attributes?.description,
+        //         image: item?.attributes?.image?.data?.attributes?.url ? `${baseUrl}${item?.attributes?.image?.data?.attributes?.url}`: null
+        //     })),
+
+        //   }));
+    
+        return { data: data };
+      },
+}));
