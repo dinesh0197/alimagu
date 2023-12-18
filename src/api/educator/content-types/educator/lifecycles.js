6e7586e1aafce718b0648d6ac4d7a808e365ed5d
dinesh <dineshr0197@gmail.com>
2023-12-18 20:09:41 +0530

    added email template for institution and educator

---
+++ src/api/educator/content-types/educator/lifecycles.js
@@ -0,0 +1,35 @@
+module.exports = {
+  afterUpdate: async ({ result }) => {
+    const { email, firstName, lastName, approve } = result;
+
+    if (approve) {
+      await strapi
+        .plugin("email-designer")
+        .service("email")
+        .sendTemplatedEmail(
+          {
+            // required
+            to: email || "alimagu@yopmail.com",
+
+            // optional if /config/plugins.js -> email.settings.defaultFrom is set
+            from: "dineshr0197@gamil.com",
+
+            // optional array of files
+            attachments: [],
+          },
+          {
+            // required - Ref ID defined in the template designer (won't change on import)
+            templateReferenceId: 1,
+
+            // If provided here will override the template's subject.
+            // Can include variables like `Thank you for your order {{= USER.firstName }}!`
+            //   subject: `Thank you for your order`,
+          },
+          {
+            // this object must include all variables you're using in your email template
+            educatorName: `${firstName || ""} ${lastName || ""}`,
+          }
+        );
+    }
+  },
+};
